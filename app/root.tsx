b5f45d07847da0cf4806c7e261510c8095ce34b0
daniel123gt <dailgozalez@gmail.com>
2026-02-18 15:23:27 -0500

    Add lab time

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 import Loading from "./components/root/Loading/Loading";
 import { Toaster } from "~/components/ui/sonner";
+import { VersionUpdateBanner } from "~/components/VersionUpdateBanner";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -39,6 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Loading />
         {children}
         <Toaster richColors position="top-center" theme="light" />
+        <VersionUpdateBanner />
         <ScrollRestoration />
         <Scripts />
       </body>
