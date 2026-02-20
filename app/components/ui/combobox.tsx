b636838fe9a30455a3190d8f60e4b2b7448e46bc
daniel123gt <dailgozalez@gmail.com>
2026-02-20 08:37:56 -0500

    Changes varios citas 2

--- app/components/ui/combobox.tsx
+++ app/components/ui/combobox.tsx
@@ -87,7 +87,13 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0" align="start">
+      <PopoverContent
+        className="w-[var(--radix-popover-trigger-width)] p-0"
+        align="start"
+        side="bottom"
+        sideOffset={4}
+        avoidCollisions={false}
+      >
         <Command shouldFilter={true}>
           <CommandInput placeholder={placeholder} />
           <CommandList>
